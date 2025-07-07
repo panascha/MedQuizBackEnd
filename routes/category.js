70908e3334dd6d839d3259141b22272d753fcec4
ArthurEZ <thanagorn2005@gmail.com>
2025-07-07 11:59:13 +0700

    Update Vercel configuration to use new API entry point and add api/index.js for request handling. Correct import path for category controller in routes/category.js.

--- routes/category.js
+++ routes/category.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getCategories,getCategoriesBySubject, getCategory, createCategory, updateCategory, deleteCategory} = require('../controllers/category');
+const { getCategories,getCategoriesBySubject, getCategory, createCategory, updateCategory, deleteCategory} = require('../controllers/Category');
 const { protect, authorize } = require('../middleware/auth');
 
 const router = express.Router();

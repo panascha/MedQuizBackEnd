9f45094f9a1d3251b45231468cc6a9ff176bc10f
Arthur <thanagorn2005@gmail.com>
2025-07-21 01:09:56 +0700

    Remove OTP rate limiting middleware from the project and update the request-reset-otp route to use authentication instead. This change simplifies the OTP request process by eliminating the rate limiter previously applied.

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const { register, login, logout, getMe, updateUser, getAllUser, checkUserExists, banUser, unbanUser, requestOTP, resetPasswordWithOTP} = require('../controllers/auth');
 const { protect, authorize } = require('../middleware/auth');
-const { otpLimiter } = require('../middleware/rateLimit');
-
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', protect, logout);
 router.get('/me', protect, getMe);
-router.post('/request-reset-otp', otpLimiter, requestOTP);
+router.post('/request-reset-otp', protect, requestOTP);
 router.post('/reset-password-otp', protect, resetPasswordWithOTP);
 router.put("/updateUser/:id", protect, authorize("S-admin","admin", "user"), updateUser);
 router.get('/users', protect, authorize('admin', 'S-admin'), getAllUser);

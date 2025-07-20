c0102dd408c6162447560bf5e522855c295ce235
Arthur <thanagorn2005@gmail.com>
2025-07-21 01:26:24 +0700

    Apply authentication middleware to reset-password-otp route in auth.js, enhancing security by requiring user authentication for password reset requests.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { register, login, logout, getMe, updateUser, getAllUser, checkUserExists, banUser, unbanUser, requestOTP, resetPasswordWithOTP} = require('../controllers/auth');
 const { protect, authorize } = require('../middleware/auth');
+const { otpLimiter } = require('../middleware/rateLimit');
+
 const router = express.Router();
 
 router.post('/register', register);
@@ -8,7 +10,7 @@ router.post('/login', login);
 router.get('/logout', protect, logout);
 router.get('/me', protect, getMe);
 router.post('/request-reset-otp', otpLimiter, requestOTP);
-router.post('/reset-password-otp', resetPasswordWithOTP);
+router.post('/reset-password-otp', protect, resetPasswordWithOTP);
 router.put("/updateUser/:id", protect, authorize("S-admin","admin", "user"), updateUser);
 router.get('/users', protect, authorize('admin', 'S-admin'), getAllUser);
 router.get('/user-exists', checkUserExists);

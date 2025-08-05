ac9493fd983d5b1ccfab866ea4814d099cd9bc11
Arthur <thanagorn2005@gmail.com>
2025-08-05 14:44:30 +0700

    Update password validation regex to require special characters in User model

--- models/User.js
+++ models/User.js
@@ -20,8 +20,8 @@ const UserSchema = new mongoose.Schema({
         minlength: [6, 'Password must be at least 6 characters'],
         select: false,
         match: [
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
-            'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character'
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$/,
+            'Password must contain at least one uppercase letter, one lowercase letter and one number'
         ]
     },
     name: {
